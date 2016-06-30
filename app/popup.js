cb2686e8226405266c7877075d7076934ae764cd
OiYouYeahYou <jason@jallan.co.uk>
2016-06-30 12:03:42 +0100

    More work
    Switching to href based links over EventHandlers
    Formatting and Dropdown Goal Selector via CSS

--- app/popup.js
+++ app/popup.js
@@ -1,46 +1,33 @@
 var TimingVariable; //= setInterval(myTimer, 1000);
-var UName, slug, LinkRefresh, LinkDatapoints, LinkSettings;
-var BeeURL = "https://www.beeminder.com/";
+var UName, slug, deadline, LinkRefresh, LinkDatapoints, LinkSettings;
+var BeeURL = "https://www.beeminder.com";
 document.addEventListener('DOMContentLoaded', PUinit);
-
 UName="OiYouYeahYou";
 slug="writing";
-
 function myTimer() {
 	var d = new Date();
 	document.getElementById("time").innerHTML =
 		d.toLocaleTimeString() +
 		" <small>Totally not a Countdown</small>";
 }
-
 function PUinit(){ //
 	TimingVariable = setInterval(myTimer, 1000);
-	document.getElementById('OptLink').addEventListener(
-		'click', OTab
-	);
-	document.getElementById('UserSelector').addEventListener(
-		'click', GOTO
-	);
+	/* TODO
+		Load data from memory
+		Load data from Beeminder
+		Populate varaibles with data
+		Set page content with data
+	*/
+	document.getElementById("ButtonGoal").href = LinkBM("");
+	document.getElementById("ButtonRefresh").href = LinkBM("refresh");
+	document.getElementById("ButtonData").href = LinkBM("datapoints");
+	document.getElementById("ButtonSettings").href = LinkBM("settings");
+	document.getElementById("OptLink").href = "/options.html";
 }
-function OTab(){
-	chrome.tabs.create({'url': '/options.html' } )
+function SetOutput(someVariable){
+	//
 }
-function GOTO(loc){
-	if (loc == undefined){loc=""}
-	link = BeeURL + UName + "/" + slug + "/" + loc
-	chrome.tabs.create({'url': link } )
-	alert("hmm")
+function ChangeGoal (GoalNumber){
+	//
 }
-/*
-GOTO
-Refresh
-data-points
-Settings
-Options
-
-
-
-
-
-
-*/
+function LinkBM(y){return BeeURL + "/" + UName + "/" + slug + "/" + y}
